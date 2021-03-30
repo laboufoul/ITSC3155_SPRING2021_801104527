f8674fed6a2920a2c3bcc4b747d2b17cd878e12d
laboufoul <laboufou@uncc.edu>
2021-03-30 13:14:15 -0400

    flask app 4 - added db connection to app

--- flask_app/flask04.py
+++ flask_app/flask04.py
@@ -6,12 +6,20 @@ from flask import Flask   # Flask is the web app that we will customize
 from flask import render_template # import render_template class
 from flask import request # gaining access to the request object in Flask
 from flask import redirect, url_for
+from database import db
 
 app = Flask(__name__)     # create an app
 notes = {1:{'title':'First note','text':'This is my first note','date':'10-1-2020'},
              2:{'title':'Second note','text':'This is my second note','date':'10-2-2020'},
              3: {'title': 'Third note', 'text': 'This is my third note', 'date': '10-3-2020'}
              }
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+#  Bind SQLAlchemy db object to this Flask app
+db.init_app(app)
+# Setup models
+with app.app_context():
+    db.create_all()   # run under the app context
 
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
