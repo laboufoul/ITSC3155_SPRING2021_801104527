136571da3b2ba852635d37f960496f64c2ce62c4
laboufoul <laboufou@uncc.edu>
2021-04-06 20:49:14 -0400

    flask app #5 - edit note function handles post and get

--- flask_app/flask05.py
+++ flask_app/flask05.py
@@ -60,14 +60,29 @@ def get_note(note_id):
     my_note = db.session.query(Note).filter_by(id=note_id).one()
     return render_template('note.html', note=my_note, user=a_user)
 
-@app.route('/notes/edit/<note_id>')
+@app.route('/notes/edit/<note_id>', methods = ['GET', 'POST'])
 def update_note(note_id):
-    # GET request - show new note form to edit note
-    # retrieve user from database
-    a_user = db.session.query(User).filter_by(email='laboufou@uncc.edu').one()
-    #retrieve note from database
-    my_note = db.session.query(Note).filter_by(id=note_id).one()
-    return render_template('new.html', note=my_note, user=a_user)
+    # check method used for request
+    if request.method == 'POST':
+        # get title data
+        title = request.form['title']
+        # get note data
+        text = request.form['noteText']
+        note = db.session.query(Note).filter_by(id=note_id).one()
+        # update note data
+        note.title = title
+        note.text = text
+        # update note in db
+        db.session.add(note)
+        db.session.commit()
+        return redirect(url_for('get_notes'))
+    else:
+        # GET request - show new note form to edit note
+        # retrieve user from database
+        a_user = db.session.query(User).filter_by(email='laboufou@uncc.edu').one()
+        #retrieve note from database
+        my_note = db.session.query(Note).filter_by(id=note_id).one()
+        return render_template('new.html', note=my_note, user=a_user)
 
 @app.route('/')
 @app.route('/index')
